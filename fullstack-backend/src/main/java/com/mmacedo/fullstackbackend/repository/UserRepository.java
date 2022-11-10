1eef020c4ad2da9e46bfe0e303cee4e08fa7a93f
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-10 19:19:53 -0300

    useRepository created

---
+++ fullstack-backend/src/main/java/com/mmacedo/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mmacedo.fullstackbackend.repository;
+
+import com.mmacedo.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+
+}
